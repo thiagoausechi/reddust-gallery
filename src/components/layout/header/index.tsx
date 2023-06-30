21187d89a9a2b24cf6edaae59fb600d740f39440
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-06-30 04:24:47 -0300

    style: :art: Organize Header class list
    Used the Prettier Tailwind Styled Component Plugin to organize all classes from the Header component.

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -34,11 +34,21 @@ interface Scrolled {
 }
 
 const Wrapper = tw.header<Scrolled>`
-  fixed z-50 flex w-full bg-black/50 backdrop-blur-lg transition-all
+  fixed
+  z-50
+  flex
+  w-full
+  bg-black/50
+  backdrop-blur-lg
+  transition-all
   ${(p) => (p.scrolled == 'true' ? 'h-14' : 'h-20')}
 `
 
 const Logo = tw.div<Scrolled>`
-  absolute left-1/2 -translate-x-1/2 transform transition-all
+  absolute
+  left-1/2
+  -translate-x-1/2
+  transform
+  transition-all
   ${(p) => (p.scrolled == 'true' ? 'top-5' : 'top-8')}
 `
