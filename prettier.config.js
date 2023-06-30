154a51e96b64a47b829a5c61e578490a4c771f9a
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-06-30 04:22:02 -0300

    chore: :wrench: Update Prettier config to use Tailwind Styled Components Plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,9 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [
+    require('prettier-plugin-tailwindcss'),
+    require('prettier-plugin-tailwind-styled-components'),
+  ],
   tailwindFunctions: ['tw'],
 
   trailingComma: 'all',
