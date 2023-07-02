bd9962f07bafd615f5b4d731bcc73b36a6338dd2
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-02 13:22:54 -0300

    fix: :lipstick: Consolidate tiles background to Tailwind
    Removed the workaround solution to a more reliable one.

--- src/components/containers/gallery-cards-section/index.tsx
+++ src/components/containers/gallery-cards-section/index.tsx
@@ -2,13 +2,12 @@ import FeatureCard from '@/layout/feature-card'
 import SectionContent from '@/layout/section-content'
 import { getFeatures } from '@/services/hygraph'
 import tw from 'tailwind-styled-components'
-import TilesBackground from '/public/images/tiles_background.png'
 
 export default async function GalleryCardsSection() {
   const features = await getFeatures()
 
   return (
-    <Wrapper style={{ background }}>
+    <Wrapper>
       <CardsList>
         {features.map((feature) => (
           <FeatureCard key={feature.slug} {...feature} />
@@ -18,21 +17,13 @@ export default async function GalleryCardsSection() {
   )
 }
 
-/**
- * I'd love to this with Tailwind, but I couldn't find any good way.
- * This is a workaround.
- */
-const DarkenMask = 'linear-gradient(0deg, #000000cc 0%, #000000cc 100%)'
-const ImageURL = `url(${TilesBackground.src}) fixed`
-const background = `${DarkenMask}, ${ImageURL}`
-
 const Wrapper = tw.section`
   relative
   min-h-screen
   w-full
   snap-start
   overflow-hidden
-  bg-repeat
+  bg-tiles
 `
 
 const CardsList = tw(SectionContent)`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        tiles: 'url("/images/tiles_background.png")',
+      },
+    },
+  },
   plugins: [],
-};
+}
